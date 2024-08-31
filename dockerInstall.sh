2d4013e6b0edc8466530023f8dad60b37716cfb9
GhouseRizvi <ghouserizvi@gmail.com>
2024-08-31 22:43:51 +0530

    Adding more files

--- dockerInstall.sh
+++ dockerInstall.sh
@@ -5,3 +5,16 @@ sudo service docker start
 sudo systemctl enable docker
 sudo usermod -a -G docker ec2-user
 sudo yum install git -y
+
+##Docker compose install
+sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+
+#NOTE: to get the latest version (thanks @spodnet): sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+
+#Fix permissions after download:
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+#Verify success:
+
+docker-compose version
